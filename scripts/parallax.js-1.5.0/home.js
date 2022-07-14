e7e8ac54623d0a7a8a927ac25b10dd81577a93d4
emmad241 <emmaduffy241@gmail.com>
2022-07-14 19:54:09 +0100

    Add contact section, style projects, write more of bio, add skills, add second image, add scroll links

--- scripts/parallax.js-1.5.0/home.js
+++ scripts/parallax.js-1.5.0/home.js
@@ -1,5 +1,23 @@
-var header = document.getElementById("header");
+$(".bio__link").click(function() {
+    $([document.documentElement, document.body]).delay(150).animate({
+      scrollTop: $("#invisible-link--bio").offset().top
+    }, 1000);
+});
 
-window.addEventListener('scroll', function(e) {
-    
+$(".skills__link").click(function() {
+    $([document.documentElement, document.body]).delay(150).animate({
+      scrollTop: $("#invisible-link--skills").offset().top
+    }, 1000);
+});
+
+$(".projects__link").click(function() {
+    $([document.documentElement, document.body]).delay(150).animate({
+      scrollTop: $("#invisible-link--projects").offset().top
+    }, 1000);
+});
+
+$(".contact__link").click(function() {
+    $([document.documentElement, document.body]).delay(150).animate({
+      scrollTop: $("#invisible-link--contact").offset().top
+    }, 1000);
 });
