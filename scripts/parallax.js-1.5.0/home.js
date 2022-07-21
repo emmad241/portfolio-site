94f3fe7a0c08f976c454174ace6a87dfa0488075
emmad241 <emmaduffy241@gmail.com>
2022-07-22 00:31:03 +0100

    Make website work on mobile, add dropdown menu

--- scripts/parallax.js-1.5.0/home.js
+++ scripts/parallax.js-1.5.0/home.js
@@ -1,23 +1,38 @@
+var hamburger = document.querySelector(".fa-bars");
+var dropdown = document.querySelector(".dropdown");
+
+hamburger.addEventListener("click", function(){
+  if(dropdown.style.display !== "none"){
+    dropdown.style.display = "none";
+  }else{
+    dropdown.style.display = "block";
+  }
+});
+
 $(".bio__link").click(function() {
     $([document.documentElement, document.body]).delay(150).animate({
       scrollTop: $("#invisible-link--bio").offset().top
     }, 1000);
+    dropdown.style.display = "none";
 });
 
 $(".skills__link").click(function() {
     $([document.documentElement, document.body]).delay(150).animate({
       scrollTop: $("#invisible-link--skills").offset().top
     }, 1000);
+    dropdown.style.display = "none";
 });
 
 $(".projects__link").click(function() {
     $([document.documentElement, document.body]).delay(150).animate({
       scrollTop: $("#invisible-link--projects").offset().top
     }, 1000);
+    dropdown.style.display = "none";
 });
 
 $(".contact__link").click(function() {
     $([document.documentElement, document.body]).delay(150).animate({
       scrollTop: $("#invisible-link--contact").offset().top
     }, 1000);
+    dropdown.style.display = "none";
 });
